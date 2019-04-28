18c82cf4cf893c5c5260f7df5dc5d9a63a0c9da3
Hendrik Feddersen <h.feddersen@mac.com>
2019-04-28 17:06:47 +0200

    Merging and reinitiating git

--- docs/scripts/11-inference-for-regression.R
+++ docs/scripts/11-inference-for-regression.R
@@ -1,26 +1,3 @@
-## ----setup_inference_regression, include=FALSE---------------------------
-chap <- 11
-lc <- 0
-rq <- 0
-# **`r paste0("(LC", chap, ".", (lc <- lc + 1), ")")`**
-# **`r paste0("(RQ", chap, ".", (rq <- rq + 1), ")")`**
-
-knitr::opts_chunk$set(
-  tidy = FALSE, 
-  out.width = '\\textwidth', 
-  fig.height = 4,
-  warning = FALSE
-  )
-
-options(scipen = 99, digits = 3)
-
-# Set random number generator see value for replicable pseudorandomness. Why 76?
-# https://www.youtube.com/watch?v=xjJ7FheCkCU
-set.seed(76)
-
-
-
-
 ## ---- message=FALSE, warning=FALSE---------------------------------------
 library(ggplot2)
 library(dplyr)
