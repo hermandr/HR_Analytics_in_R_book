f51a73dc8d7917c23ebae97c86b7b98a5fe64572
Hendrik Feddersen <h.feddersen@mac.com>
2019-05-11 17:46:01 +0100

    Cleaned file names

--- copy_pdf_to_google_drive.R
+++ copy_pdf_to_google_drive.R
@@ -1,6 +1,6 @@
 library(googledrive)
 
-drive_upload(media = "docs/ismaykim.pdf",
+drive_upload(media = "docs/hendrikfeddersen.pdf",
              path = "HR_Analytics_live_book_in_pdf/",
              name = paste0("HR_Analytics_live_book_", Sys.Date(), ".pdf"),
              type = NULL,
